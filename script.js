c84191420a0c580ed643fb733af0b871fea9a87f
01smurat <karl.email999@gmail.com>
2026-04-09 20:37:14 +0200

    Create function to execute operation & add variables

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, operator, num2) {
+    return operator === "+" ? num1 + num2
+        : operator === "-" ? num1 - num2
+        : operator === "*" ? num1 * num2
+        : operator === "/" ? num1 / num2
+        : "";
+}
