b7466dad65fd74368bdf6a231ae4c9a454b386d8
01smurat <karl.email999@gmail.com>
2026-04-10 05:38:07 +0200

    Run operations with equal sign & tidy up code

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-function add(a, b) {
-    return a + b;
-}
+// math functions
+function add(a, b) {return a + b}
+function subtract(a, b) {return a - b}
+function multiply(a, b) {return a * b}
+function divide(a, b) {return a / b}
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
+// variables to hold data
 let num1;
 let num2;
 let operator;
+let answer;
 
-function operate(num1, operator, num2) {
-    return operator === "+" ? num1 + num2
-        : operator === "-" ? num1 - num2
-        : operator === "*" ? num1 * num2
-        : operator === "/" ? num1 / num2
-        : "";
+// runs the math operation
+function operate() {
+    answer = operator === "+" ? +num1 + +num2
+            : operator === "-" ? +num1 - +num2
+            : operator === "*" ? +num1 * +num2
+            : operator === "/" ? +num1 / +num2
+            : "error";
+
+    display.textContent = answer;
+    num2 = answer;
 }
 
+let equalTo = document.querySelector("#equal");
+equalTo.addEventListener("click", operate)
+
+
+// Shows digits on display
 const display = document.querySelector(".display");
 display.textContent = "";
 
-function updateDisplay(e) {
-    display.textContent += e.target.textContent;
-}
-
 let digits = document.querySelectorAll(".number");
 digits.forEach(digit => digit.addEventListener("click", updateDisplay))
+
+function updateDisplay(e) {
+    display.textContent += e.target.textContent;
+    num2 = display.textContent;
+}
+
+
+// Runs code when operator is clicked
+let operators = document.querySelectorAll(".operators");
+operators.forEach(op => op.addEventListener("click", useOperator))
+
+function useOperator(e) {
+    num1 = num2;
+    num2 = "";
+    operator = e.target.textContent;
+    display.textContent = "";
+}
